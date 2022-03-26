fbe7b9e8f9d07292da6d67c6ad5b6c130ea4e49c
Leonid <dolgodvorov.l@yandex.ru>
2022-03-26 17:09:05 +0500

    Устранены замечания после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,30 @@ const profileEditButton = document.querySelector ('.profile__edit');
 const closeProfileEditButton = popupElement.querySelector('.popup__closebutton');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const popupName = popupElement.querySelector('.popup__name');
-const popupJob = popupElement.querySelector('.popup__job');
-const ESC_KEY = "Escape";
+const popupName = popupElement.querySelector('.popup__input_type_name');
+const popupJob = popupElement.querySelector('.popup__input_type_job');
+/* const ESC_KEY = "Escape"; */
 const popupForm = document.querySelector('.popup__form');
-const likeButton = document.querySelector('.elements__likebutton');
+/* const likeButton = document.querySelector('.elements__likebutton'); */
 
 
 function openPopup () {
   popupElement.classList.add('popup_opened');
   popupName.value = profileName.textContent;
   popupJob.value = profileJob.textContent;
-  document.addEventListener('keyup', onDocumentKeyUp);
+/*   document.addEventListener('keyup', onDocumentKeyUp); */
 }
 
 function closePopup () {
   popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keyup', onDocumentKeyUp);
+/*   document.removeEventListener('keyup', onDocumentKeyUp); */
 }
 
-function onDocumentKeyUp(event){
+/* function onDocumentKeyUp(event){
   if (event.key === ESC_KEY) {
       closePopup();
   }
-}
+} */
 
 function saveProfileInfo (evt) {
   evt.preventDefault();
@@ -35,11 +35,11 @@ function saveProfileInfo (evt) {
   closePopup ();
 }
 
-function touchLike () {
+/* function touchLike () {
   likeButton.classList.toggle('elements__likebutton_yes');
-}
+} */
 
 profileEditButton.addEventListener('click', openPopup);
 closeProfileEditButton.addEventListener('click', closePopup);
-likeButton.addEventListener('click', touchLike);
+/* likeButton.addEventListener('click', touchLike); */
 popupForm.addEventListener('submit', saveProfileInfo);
