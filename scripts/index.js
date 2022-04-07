20a52c24e32f7ef796ba9ffd701308130c5be7d2
Leonid <dolgodvorov.l@yandex.ru>
2022-04-07 15:15:25 +0500

    addFeature: добавление карточек из массива, лайки, кнопка удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Тель-Авив',
+    link: 'https://images.unsplash.com/photo-1500990702037-7620ccb6a60a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
   },
   {
     name: 'Челябинская область',
@@ -25,29 +25,35 @@ const initialCards = [
   }
 ]; 
 
-const popupElement = document.querySelector ('.popup');
+const popup = document.querySelector ('.popup');
 const profileEditButton = document.querySelector ('.profile__edit');
-const closeProfileEditButton = popupElement.querySelector('.popup__closebutton');
+const profilePopup = document.querySelector('.popup_type_profile');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const popupName = popupElement.querySelector('.popup__input_type_name');
-const popupJob = popupElement.querySelector('.popup__input_type_job');
+const popupInputName = popup.querySelector('.popup__input_type_name');
+const popupInputJob = popup.querySelector('.popup__input_type_job');
+const popupInputPlace = popup.querySelector('.popup__input_type_place');
+const popupInputPlaceLink = popup.querySelector('.popup__input_type_place-link');
+const addPlacePopup = document.querySelector('.popup_type_addplace');
 /* const ESC_KEY = "Escape"; */
-const popupForm = document.querySelector('.popup__form');
-/* const likeButton = document.querySelector('.elements__likebutton'); */
+const popupFormName = document.querySelector('.popup__form_type_name');
+const popupFormPlace = document.querySelector('.popup__form_type_place');
+const imagePopup = document.querySelector('.popup_type_image');
+const elementsList = document.querySelector('.elements__list');
+const addElement = document.querySelector('.profile__addbutton');
+const popupPic = document.querySelector('.popup__image');
+const popupPicText = document.querySelector('.popup__image-text');
 
 
-function openPopup () {
-  popupElement.classList.add('popup_opened');
-  popupName.value = profileName.textContent;
-  popupJob.value = profileJob.textContent;
-/*   document.addEventListener('keyup', onDocumentKeyUp); */
-}
+const openPopup = (x) => () => { 
+  x.classList.add('popup_opened');
+  const closeButton = popup.querySelector('.popup__closebutton');
+  closeButton.addEventListener("click", closePopup(x));
+};
 
-function closePopup () {
-  popupElement.classList.remove('popup_opened');
-/*   document.removeEventListener('keyup', onDocumentKeyUp); */
-}
+const closePopup = (x) => () => {  
+  x.classList.remove('popup_opened');
+};
 
 /* function onDocumentKeyUp(event){
   if (event.key === ESC_KEY) {
@@ -55,18 +61,50 @@ function closePopup () {
   }
 } */
 
-function saveProfileInfo (evt) {
-  evt.preventDefault();
-  profileName.textContent = popupName.value;
-  profileJob.textContent = popupJob.value;
-  closePopup ();
-}
+profileEditButton.addEventListener('click', () => {
+  openPopup(profilePopup)();
+  popupInputName.value = profileName.textContent;
+  popupInputJob.value = profileJob.textContent;
+});
 
-/* function touchLike () {
-  likeButton.classList.toggle('elements__likebutton_yes');
-} */
+function saveProfileInfo (e) {
+  e.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileJob.textContent = popupInputJob.value;
+  closePopup(profilePopup)();
+};
 
-profileEditButton.addEventListener('click', openPopup);
-closeProfileEditButton.addEventListener('click', closePopup);
-/* likeButton.addEventListener('click', touchLike); */
-popupForm.addEventListener('submit', saveProfileInfo);
+/* popupFormName.addEventListener('submit', saveProfileInfo); */
+
+function touchLike (e) {
+  e.target.classList.toggle('elements__likebutton_yes');
+};
+
+function deleteElement (e) {
+  e.target.closest(".elements__element").remove();
+};
+
+const createElement = (x) => {
+  const template = document.querySelector("#element-template");
+  const newElement = template.content.querySelector(".elements__element").cloneNode(true);
+  const likeButton = newElement.querySelector('.elements__likebutton');
+  const deleteButton = newElement.querySelector(".elements__deletebutton");
+  const elementPic = newElement.querySelector(".elements__pic");
+
+  newElement.querySelector(".elements__name").textContent = x.name;
+  newElement.querySelector(".elements__pic").src = x.link;
+  newElement.querySelector(".elements__pic").alt = `Картинка ` + x.name;
+   
+  likeButton.addEventListener("click", touchLike);
+  deleteButton.addEventListener("click", deleteElement);
+  elementPic.addEventListener('click', () => {});
+  return newElement;
+};
+
+const renderElement = (x) => {
+  elementsList.prepend(x);
+};
+
+initialCards.forEach((x) => renderElement(createElement(x)));
+
+addElement.addEventListener("click", openPopup(addPlacePopup));
